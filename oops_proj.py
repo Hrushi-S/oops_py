50e55fb57b0a34a50bb44d7be219b0cc15453532
hrushi <hrushi.s@outlook.com>
2025-12-28 18:14:09 -0600

    added oops_pro.py

---
+++ oops_proj.py
@@ -0,0 +1,31 @@
+class chatbook:
+    def __init__(self):
+        self.username = ""
+        self.password = ""
+        self.logged_in = False
+        self.menu()
+
+    def menu(self):
+        while True:
+            print("\n--- ChatBook Menu ---")
+            print("1. Sign Up")
+            print("2. Log In")
+            print("3. Post a Message")
+            print("4. View Messages")
+            print("Any other key to Exit")
+            choice = input("Choose an option: ")
+
+            if choice == '1':
+                self.sign_up()
+            elif choice == '2':
+                self.log_in()
+            elif choice == '3':
+                self.post_message()
+            elif choice == '4':
+                self.view_messages()
+            else:
+                print("Exiting ChatBook. Goodbye!")
+                break
+
+
+obj = chatbook()
